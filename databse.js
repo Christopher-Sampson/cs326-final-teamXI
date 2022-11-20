befa9ec78d16362f2310958b898fbc442636aac1
Bluboi404 <113072996+Bluboi404@users.noreply.github.com>
2022-11-19 19:37:27 -0500

    organizing

--- databse.js
+++
@@ -1,2 +0,0 @@
-/*This will be part of milestone 3 and its to have basic functions that actually interact with the databases and
-perform operations to find, update, remove, and insert objects into the respective databases, preety much implement CRUD*/
---
+++ server/databse.js
@@ -0,0 +1,13 @@
+/*This will be part of milestone 3 and its to have basic functions that actually interact with the databases and
+perform operations to find, update, remove, and insert objects into the respective databases, preety much implement CRUD*/
+
+
+
+//create function
+
+
+//read function
+
+//update function
+
+//delete function
